a46951c7cab682b38b104ef7aa3beaf51ed5d9e8
kishan-developer <139308962+kishan-developer@users.noreply.github.com>
2024-07-14 19:40:30 +0530

    Update AppLayout.jsx

--- src/Ui/AppLayout.jsx
+++ src/Ui/AppLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../Components/Navbar/Navbar';
 import Header from '../Components/Header/Header'
-import Product from '../Pages/ProductPage/Product';
+// import Product from '../Pages/ProductPage/Product';
 import SecNav from '../Components/Navbar/SecNav';
 import Footer from '../Components/Footer/Footer';
 // import AppLayout from '../Ui/AppLayout'
